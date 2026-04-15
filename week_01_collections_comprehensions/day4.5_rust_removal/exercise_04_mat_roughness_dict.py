a95c1d9dde09fb8a7d39dbff9795ce408dfdb916
Iamren-dot-art <arian@renllc.com>
2026-04-14 21:26:12 -0700

    merge: sync day 4.5, rust removal exercises

--- week_01_collections_comprehensions/day4.5_rust_removal/exercise_04_mat_roughness_dict.py
+++ week_01_collections_comprehensions/day4.5_rust_removal/exercise_04_mat_roughness_dict.py
@@ -1,20 +1,25 @@
-'''
+"""
 Your Task: Write a dictionary comprehension using zip() that maps the material to its roughness, but only if the roughness is strictly greater than 0.0. Print the resulting dictionary.
 (Hint: Your blueprint is { [KEY]: [VALUE] for [KEY], [VALUE] in zip(...) if [CONDITION] })
-'''
+"""
+
+# [ [ACTION] for [ITEM] in [LIST] if [CONDITION] ]
 
 # Data
 materials = ["Wood", "Metal", "Glass"]
 roughness = [0.8, 0.4, 0.0]
 
 
-'''
+"""
 # Standard Loop
 rough_mats = {}
 
 for mat, rough in zip(materials, roughness):
     if rough > 0.0:
         rough_mats[mat] = rough
+"""
+
+# Comprehension
+rough_mats = {mat: rough for mat, rough in zip(materials, roughness) if rough > 0.0}
 
 print(rough_mats)
-'''
